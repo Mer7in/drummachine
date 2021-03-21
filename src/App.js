0d09f916f4a1b4644ad0a1789a6c02364ac07b31
Mer7in <39715126+Mer7in@users.noreply.github.com>
2021-03-21 12:55:32 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
+function outputUpdate(vol) {
+  document.querySelector("#volume").value = vol;
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 0,
+    };
+
+    this.handleVolume = this.handleVolume.bind(this);
+  }
+
+  handleVolume = (event) => {
+    this.setState({ value: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div id="drum-machine">
+          <div className="container">
+            <div id="drum-config">
+              <div className="column">
+                <div className="toggle-btn">
+                  <input
+                    type="checkbox"
+                    className="toggle"
+                    id="poweroff"
+                  ></input>
+                  <label className="slider-poweroff" htmlFor="poweroff"></label>
+                  <span className="tooltiptext power">Power on</span>
+                </div>
+
+                <div className="toggle-btn">
+                  <input type="checkbox" className="toggle" id="bank"></input>
+                  <label className="slider-bank" htmlFor="bank"></label>
+                  <span className="tooltiptext bank">Bank</span>
+                </div>
+              </div>
+              <div className="column">
+                <div id="drum-name">
+                  <span>
+                    <strong>No Drum</strong>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div id="drum-pad-container">
+              <div className="row">
+                <a href="#" className="drum-pad">
+                  Q
+                </a>
+                <a href="#" className="drum-pad">
+                  W
+                </a>
+                <a href="#" className="drum-pad">
+                  E
+                </a>
+              </div>
+              <div className="row">
+                <a href="#" className="drum-pad">
+                  A
+                </a>
+                <a href="#" className="drum-pad">
+                  S
+                </a>
+                <a href="#" className="drum-pad">
+                  D
+                </a>
+              </div>
+              <div className="row">
+                <a href="#" className="drum-pad">
+                  Z
+                </a>
+                <a href="#" className="drum-pad">
+                  X
+                </a>
+                <a href="#" className="drum-pad">
+                  C
+                </a>
+              </div>
+            </div>
+            <div id="volume-box" className="column">
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value={this.state.value}
+                id="volume"
+                step="1"
+                onChange={this.handleVolume}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function Pad({ clip }) {
+  const playSound = () => {
+    const audioTag = document.getElementById(clip.keyTrigger);
+    audioTag.currentTime = 0;
+    audioTag.play();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <a href="#" className="drum-pad">
+      <audio className="clip" id={clip.keyTrigger} src={clip.url} />
+    </a>
   );
 }
 
